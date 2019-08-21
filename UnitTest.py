a84cde8e767f6fe0f2d421555793a0bcb141f020
Volkan Tolu <volkantolu@gmail.com>
2019-08-21 12:22:22 +0300

    Update UnitTest.py

--- UnitTest.py
+++ UnitTest.py
@@ -1,10 +1,31 @@
+cat <<-'TEST_CASES' > test.py
+#!/usr/bin/env python
 import unittest
+import app
 
+class TestHello(unittest.TestCase):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
 
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_hello(self):
+        rv = self.app.get('/hello/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_name(self):
+        name = 'Simon'
+        rv = self.app.get(f'/hello/{name}')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
     unittest.main()
+TEST_CASES
+chmod +x test.py
